37e4929378aeab651b1fb9679ac416e4fea667ad
Ruize Nie <rz376775@dal.ca>
2021-09-08 19:56:29 -0300

    project uploaded

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kYellowLight = Color(0xFFFFF7EC);
+const Color kYellow = Color(0xFFFAF0DA);
+const Color kYellowDark = Color(0xFFEBBB7F);
+
+const Color kRedLight = Color(0xFFFCF0F0);
+const Color kRed = Color(0xFFFBE4E6);
+const Color kRedDark = Color(0xFFF08A8E);
+
+const Color kBlueLight = Color(0xFFEDF4FE);
+const Color kBlue = Color(0xFFE1EDFC);
+const Color kBlueDark = Color(0xFFC0D3F8);
